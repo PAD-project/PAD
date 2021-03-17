b9974850603777d1893771f67e558fc69ccc9d31
Scrolls5 <56028311+scrolls5@users.noreply.github.com>
2021-03-17 14:47:11 +0100

    mysql connection tester

---
+++ Docker Compose/test.php
@@ -0,0 +1,13 @@
+<!-- Test .php website om de MySql connectie te testen -->
+<?php
+$host = 'db';
+$user = 'root';
+$password = 'ssd';
+$db = 'TEST';
+
+$conn = new mysqli($host,$user,$password,$db);
+if($conn->connect_error){
+  echo 'connection failed' . $conn->connect_error;
+}
+echo 'Succesfully connected to MYSQL';
+?>
