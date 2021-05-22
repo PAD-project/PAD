c3b0d682f7cdbe6a18e305a81b5fd3dc2c01e41b
maxklaver <max.klaver@hva.nl>
2021-05-22 14:16:12 +0200

    FIx PYS challenge.

--- database/sql/credentials_ENCRYPTED.sql
+++ database/sql/credentials_ENCRYPTED.sql
@@ -17,10 +17,14 @@ CREATE TABLE `credentials_ENCRYPTED` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `credentials_ENCRYPTED` (`username`, `password`) VALUES
-('5ffd0fb60306475563b9a1890b395f36',	'b13d4bb1aac47bc80625872301045025'),
-('8a034eba2276afb3f95d2d1651a16a55',	'd51aa55bdc95869486149b9e029c3a64'),
-('8899158445846b703defc362c35c43f0',	'59a9e477c8b5cd962746ec17f0bd2fa3'),
-('4b93c9ac40c218a6b76c46b32e2692d1',	'272d62ef9c9dea3932df1332436911fb');
+('desperate_enuf',	'6b21b2958b3ce2043ac78b1c1dcda5fd'),
+('stinky_pinky',	'e7ffec49eb01e2dab35c19fe5b6969a1'),
+('bad_karma',	'd615e0078551be8111ad222380b2cdbc'),
+('harry_hond',	'272d62ef9c9dea3932df1332436911fb'),
+('olie_gopolie',	'bafd00e88d3bc2053298e90d6c1a32e3'),
+('karel_kat',	'02634eff3c24742057ecacbee91c4bbd'),
+('average_student',	'9246f080c855a69012707ab53489b921'),
+('butt_smasher',	'90cc74d5256f614fc6658cf7942dadd9');
 
 CREATE USER 'beeste_bende'@'%' IDENTIFIED BY 'ikhouvantennisballen';
 GRANT SELECT ON `credentials\_ENCRYPTED`.* TO 'beeste_bende'@'%';
